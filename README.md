055422cbae7364fa1f509dc3f763ec378bf1ce3f
2betop <liaoxuezhi@icloud.com>
2015-09-24 13:02:41 +0800

    update

---
+++ README.md
@@ -0,0 +1,48 @@
+## fis-parser-typescript
+
+typescript parser for fis
+
+
+### INSTALL
+
+```bash
+npm install -g fis-parser-typescript
+```
+
+### USEAGE
+
+	
+	```js
+	fis.match('/es6/**.ts', {
+		parser: fis.plugin('typescript', {
+				// options
+			}),
+		rExt: '.js'
+	});
+	```
+	
+### 注意
+
+如果你的 fis 版本小于 3.2.2 请加以下配置。
+
+
+```js
+fis.set('project.fileType.text', 'ts,tsx');
+```
+
+### 常用配置说明
+
+* `jsx` 默认为 2, 解析 `<xml>` 为对应的  `react` 语句。如果设置 1, 则保留。
+* `module` 默认为 1.
+
+	* `1`: commonjs
+  * `2`: amd
+  * `3`: umd
+  * `4`: system
+* `target` 默认为 0, es3.
+	
+	* `0`: es3
+	* `1`: es5
+  * `2`: es6
+* `sourceMap` 默认为 `false` 配置是否输出 sourcemap
+
---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * fis.baidu.com
+ */
+
+var ts = require('typescript');
+var util = require('util');
+ 
+module.exports = function (content, file, opts) {
+  var conf = fis.util.clone(opts);
+
+  var result = ts.transpileModule(content, {
+    compilerOptions: conf,
+    fileName: file.realpath,
+    reportDiagnostics: true,
+    moduleName: undefined
+  });
+
+
+  result.diagnostics.forEach(function(e) {
+    var msg = util.format('Syntax Error: %s in `%s`', e.messageText, file.subpath);
+    fis.log.warning(msg);
+  });
+
+  if (result.sourceMapText) {
+    var mapping = fis.file.wrap(file.dirname + '/' + file.filename + file.rExt + '.map');
+
+    // 修改 source 文件名。
+    var sourceMapObj = JSON.parse(result.sourceMapText);
+    sourceMapObj.sources = [file.subpath];
+    result.sourceMapText = JSON.stringify(sourceMapObj, null, 4);
+
+    mapping.setContent(result.sourceMapText);
+    var url = mapping.getUrl(fis.compile.settings.hash, fis.compile.settings.domain);
+    
+    result.outputText = result.outputText.replace(/\n?\s*\/\/#\ssourceMappingURL=.*?(?:\n|$)/g, '');
+    result.outputText += '\n//# sourceMappingURL=' +  url + '\n';
+
+    file.extras = file.extras || {};
+    file.extras.derived = file.extras.derived || [];
+    file.extras.derived.push(mapping);
+  }
+  
+  return result.outputText;
+};
+
+module.exports.defaultOptions = {
+  // 1: preserve
+  // 2: React
+  jsx: 2,
+  
+  // 1: commonjs
+  // 2: amd
+  // 3: umd
+  // 4: system
+  module: ts.ModuleKind.CommonJS,
+
+  // 0: ES3
+  // 1: ES5
+  // 2: ES6
+  target: ts.ScriptTarget.ES3,
+  noImplicitAny: false,
+  outDir: "built",
+  rootDir: fis.project.getProjectPath(),
+  sourceMap: false,
+  inlineSources: true
+};
